7d2ab9bacb68b82105564c7a1170fbca9737c8c4
Said Mavlyan <Said.Mavlyan@gmail.com>
2016-07-10 03:42:22 +0600

    ++

--- mail.php
+++ mail.php
@@ -1,10 +1,13 @@
-<?php
-$to      = 'nobody@example.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+<?php 
+$to      = "test@nk-service.biz";
+$subject = "kymyz.kg'den kat";
+$message = "Аты: " . filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+$message .= "\r\n\r\nE-mail: " . filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+$message .= "\r\n\r\n" . filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+
+$headers = "From: webmaster@kymyz.kg" . "\r\n" . "Reply-To: webmaster@kymyz.kg";
 
 mail($to, $subject, $message, $headers);
-?>
+
+header("Location: " . "index.html");
+
